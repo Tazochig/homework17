c5545c29cca52253980296a4ac37374a31ddd884
Tazochig <chigladzetazo@gmail.com>
2024-10-28 18:05:59 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,127 +1,93 @@
+// arrow funqciebi
 1
-function multiply(a, b) {
- return a * b;   
-}
-2
-function checkAge(age) {
-    if ( age >= 18 ) {
-        return "სრულწლოვანი"
-    }
-    else { return "არასრულწლოვანი"
-    }
-}
+const add = (a, b) => a + b;
+console.log(add(15,1));
 3
-function isEvenOrOdd(num) {
-    if ( num % 2 == 0) {
-        return "ლუწი"
-    }
-    else { return "კენტი"}
-}
-4
-switch (true) {
-    case 1: return "ორშაბათი";
-    case 2: return "სამშაბათი";
-    case 3: return "ოთხშაბათი";
-    case 4: return "ხუთშაბათი";
-    case 5: return "პარასკევი";
-    case 6: return "შაბათი";
-    case 7: return "კვირა";
-}
+const even = (num) => num % 2 === 0;
+console.log(even(54));
 5
-function compareNumbers(a, b) {
-    if ( a > b) {
-        return "პირველი რიცხვი უფრო დიდია"
-    }
-    else if ( a < b) {
-        return "მეორე რიცხვი უფრო დიდია"
-    }
-    else if ( a == b) {
-        return "ორივე რიცხვი ტოლია"
-    }
-}
-6
-function calculator(a, b, operator) {
-    if (operator === '+') {
-        return a + b;
-    } else if (operator === '-') {
-        return a - b;
-    } else if (operator === '*') {
-        return a * b;
-    } else if (operator === '/') {
-        return a / b;
-    }
-}
-7
-function celsiusToFahrenheit(celsius) {
-    // პირობა ვერ გავიგე
-}
+const positive = (num) => num >= 0;
+console.log(positive(-12));
 8
-function findMax(a, b, c) {
-    // ვერ გავაკეთე
-}
-9
-function checkSign(num) {
-    if (num >= 0) {
-        return "დადებითი"
-    }
-    else {
-        return "უარყოფითი"
-    }
-}
+const isDivisibleByThree = (num) => num % 3 === 0;
+console.log(isDivisibleByThree(72))
 10
-switch (true) {
-    case 1: return "იანვარი";
-    case 2: return "თებერვალი";
-    case 3: return "მარტი";
-    case 4: return "აპრილი";
-    case 5: return "მაისი";
-    case 6: return "ივნისი";
-    case 7: return "ივლისი";
-    case 8: return "აგვისტო";
-    case 9: return "სექტემბერი";
-    case 10: return "ოქტომბერი";
-    case 11: return "ნოემბერი";
-    case 12: return "დეკემბერი";
-}
-11
-function multiplyIfGreaterThanFive(num) {
-    if( num >= 5 ) {
-        return num * 10
-    }
-    else {
-        return "რიცხვი მცირეა"
-    }
-}
+const cube = (num) => num * num * num;
+console.log(cube(5))
 12
-switch (true) {
-    case 1: return "გაზაფხული";
-    case 2: return "ზაფხული";
-    case 3: return "შემოდგომა";
-    case 4: return "ზამთარი";
-}
-13
-function checkPrice(price) {
-    if ( price > 100)
-        return "ფასი მაღალია"
-    else {
-        return "ფასი ნორმალურია"
-    }
-}
-14
-function calculator(a, b, operator) {
-    switch (operator) {
-        case 1: return a + b;
-        case 2: return a - b;
-        case 3: return a * b;
-        case 4: return a / b;
-    }
-}
+const isGreaterThanZero = (num) => num > 0;
+console.log(isGreaterThanZero(-59)    )
 15
-function positiveSum(a, b) {
-    if (a > 0 && b > 0){
-        return a + b; 
-    }
-    else {
-        return "მინიმუმ ერთი რიცხვი უარყოფითია"
-    }
+const square = (num) => num * num
+console.log(square(12))
+
+// anonimuri funqcia/ callback funqcia
+2
+const gamokleba = function(num1,num2) {
+  return num1 - num2; 
+};
+console.log(gamokleba(1122,1))
+
+4
+
+ function multiplication( num1 , num2, callback){
+    return callback(num1, num2);
+ }
+ function gamrvaleba(a, b) {
+    return a * b; 
+ }
+ const result = multiplication( 31, 6, gamrvaleba);
+ console.log(result);
+
+6
+
+  const gaormageba = function(n) {
+    return n * 2;
+  };
+  console.log(gaormageba(5)); 
+
+7
+
+  function doubleNumber(number, callback) {
+    const result = number * 2;
+    callback(result);
+  }
+  function shedegi(result) {
+    console.log(result);
+  }
+  doubleNumber(5, shedegi);
+9
+
+  function checkifodd( number, callback) {
+  const result = number % 2 === 0;
+  callback(result);
 }
+function shedegi(result) {
+  console.log(result);
+}
+{checkifodd(7, shedegi)}
+
+11
+  function gamravleba ( num1, num2, callback) {
+  const result = num1 * num2;
+  callback(result)
+}
+function shedegi(result) {
+  console.log(result)
+  }
+  gamravleba(11,3,shedegi)
+  13
+    const divide = function(n) {
+    return n / 2;
+  };
+  console.log(divide(72));
+
+14
+  function shejameba( num1, num2, callback) {
+    const result = num1 + num2;
+    callback(result);
+  }
+  function shedegi(result) {
+    console.log(result);
+  }
+  {shejameba(1000, 500, shedegi)}
